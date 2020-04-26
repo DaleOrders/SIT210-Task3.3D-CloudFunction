59ee5809e60ed5c1d0de546f4c32129100cb52f3
DaleOrders <52617475+DaleOrders@users.noreply.github.com>
2020-04-26 19:10:38 +1000

    Add files via upload

---
+++ task-3-3.ino
@@ -0,0 +1,66 @@
+int blue = D5;
+
+
+
+void eventPublished(const char *event, const char *data);
+
+void setup()
+{
+    Particle.subscribe("Deakin_RIOT_SIT210_Photon_Buddy", eventPublished);
+    
+    pinMode(blue, OUTPUT);
+    digitalWrite(blue, LOW);
+
+}
+
+void eventPublished(const char *event, const char *data)
+{
+    
+
+if (strcmp(data, "wave") == 0)
+{
+//  wave
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);    
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100); 
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);    
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);   
+}
+else if (strcmp(data, "pat") == 0)
+{
+    //2 blinks for pat
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);    
+    digitalWrite(blue, HIGH);
+    delay(100);
+    digitalWrite(blue, LOW);
+    delay(100);
+
+}
+
+}
+void loop()
+{
+
+}
